81a27bc3615f8ce10d8446928b9ae4f4c8a54aec
Andrew Straw <strawman@astraw.com>
2023-09-12 10:52:55 +0200

    remove unused linux headers

--- led_clock.c
+++ led_clock.c
@@ -8,17 +8,8 @@
 *  \Tested with Linux raspberrypi 6.1.21-v7+
 *
 *******************************************************************************/
-#include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kdev_t.h>
-#include <linux/fs.h>
 #include <linux/cdev.h>
-#include <linux/device.h>
-#include <linux/delay.h>
-#include <linux/uaccess.h>  //copy_to/from_user()
-#include <linux/gpio.h>     //GPIO
-#include <linux/err.h>
+#include <linux/gpio.h>
 
 // LED is connected to this GPIO
 #define GPIO_LED (16)
